8d921139b7699f61ffcb41c898cf95cc2128de95
DwiNabela <dwinabela0910@gmail.com>
2022-03-18 06:15:14 -0700

    Praktikum1

---
+++ Modul1/PRAK101.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Hello World in PHP <br>";
+    echo "Nama Praktikan : Dwi Nabela <br>";
+    echo "NIM : 2010817120009"
+?>
---
+++ Modul1/PRAK102.php
@@ -0,0 +1,8 @@
+<?php
+    $panjang = 8.9;
+    $lebar = 14.7;
+    $tinggi = 5.4;
+
+    $volumeLimas = 1/3 * $panjang * $lebar * $tinggi;
+    echo number_format($volumeLimas, 3, ",")," m3"
+?>
